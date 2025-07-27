f104907d3d1d137a64c7ae576c3c6bce01adf007
eagleevolutioncorp <info@eevolutioncorp.com>
2025-07-26 20:54:01 -0500

    Update app.py
    New cambios de carflax

--- app.py
+++ app.py
@@ -2,14 +2,17 @@ from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
-from wtforms import IntegerField, StringField
+from wtforms.fields import StringField, IntegerField
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///vehicles.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'mysecret'
+
 db = SQLAlchemy(app)
 admin = Admin(app, name='Carfax Panamá', template_mode='bootstrap3')
 
+# Modelo
 class Vehicle(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     plate = db.Column(db.String(10), unique=True, nullable=False)
@@ -22,6 +25,7 @@ class Vehicle(db.Model):
     def __repr__(self):
         return f'<Vehicle {self.plate}>'
 
+# Vista personalizada
 class VehicleView(ModelView):
     form_overrides = {
         'plate': StringField,
@@ -44,19 +48,22 @@ class VehicleView(ModelView):
     form_create_rules = ['plate', 'make', 'model', 'year', 'accidents', 'owners']
     form_edit_rules = form_create_rules
 
+# Registro en admin
 admin.add_view(VehicleView(Vehicle, db.session))
 
+# Inicializa DB y datos de prueba
 with app.app_context():
     db.create_all()
     if not Vehicle.query.first():
-        vehicles = [
+        demo_vehicles = [
             Vehicle(plate='AB1234', make='Toyota', model='Corolla', year=2020, accidents=0, owners=1),
             Vehicle(plate='CD5678', make='Honda', model='Civic', year=2019, accidents=1, owners=2),
             Vehicle(plate='EF9012', make='Ford', model='Fusion', year=2018, accidents=0, owners=1)
         ]
-        db.session.bulk_save_objects(vehicles)
+        db.session.bulk_save_objects(demo_vehicles)
         db.session.commit()
 
+# Ruta principal
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -67,5 +74,7 @@ def report():
     vehicle = Vehicle.query.filter_by(plate=plate).first()
     return render_template('report.html', report=vehicle)
 
+# Inicia app
 if __name__ == '__main__':
     app.run(debug=True)
+
